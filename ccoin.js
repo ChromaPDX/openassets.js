d0356482d85b2a444fc6fb0d102711bcdc2a7612
Don Park <donp@donp.org>
2015-03-19 15:24:21 -0700

    command line coloredcoin tool

---
+++ ccoin.js
@@ -0,0 +1,77 @@
+// nodejs core
+var fs = require('fs')
+var url = require('url')
+// npm modules
+var openassets = require('openassets')
+var request  = require('request')
+var cliArgs = require("command-line-args")
+
+// api keys
+var settings = JSON.parse(fs.readFileSync("settings.json"))
+var config = { url: "https://api.chain.com/v2"};
+
+/* define the command-line options */
+var cli = cliArgs([
+    { name: "help", type: Boolean, description: "Print usage instructions" },
+    { name: "words", type: Array, defaultOption: true, description: "sub-commands" }
+]);
+
+/* parse the supplied command-line values */
+var options = cli.parse();
+
+/* generate a usage guide */
+var usage = cli.getUsage({
+    header: "A colored coin tool.",
+});
+
+if(options.help || !options.words ||
+    (options.words && options.words.length == 0)) {
+  console.log(usage);
+  process.exit()
+}
+var cmd = options.words[0]
+
+// A wrapper to generate a "transaction provider" given a config.
+//
+// For generality, connection to the Bitcoin JSON-RPC service is
+// externalized into the concept of a "transaction provider" that is
+// expected to conform to the following simple API: given a Bitcoin
+// transaction hash and a callback function, the provider must
+// populate the callback with the results of the 'getRawTransaction'
+// JSON-RPC call.
+getTransactionProvider = function getTransactionProvider(config) {
+  return function transactionProvider(hash, cb) {
+    var api = url.parse(config.url)
+    api.auth = settings.key+":"+settings.secret
+    api.pathname = api.pathname+"/bitcoin/transactions/"+hash+"/hex"
+    request(url.format(api), function(err, resp, body){
+      var response = JSON.parse(body)
+      cb(err, {result:response.hex}) // body.message body.result
+    });
+  };
+};
+
+// Create an instance of the Open Assets ColoringEngine, and pass to
+// it a configured transaction provider
+ce = new openassets.ColoringEngine(getTransactionProvider(config));
+
+// Use the coloring engine to obtain information about a transaction. In
+// this case, get the 0th output of a known Open Assets 'issuance' transaction.
+// The first argument is the hash of the transaction, the 2nd is the index
+// of the output to retrieve, and the third is a callback function that will
+// be populated with the asset ID and asset quantity information, if any, associated with
+// that output.
+ce.getOutput(
+  '77a6bbc65aa0326015835a3813778df4a037c15fb655e8678f234d8e2fc7439c',
+  0, function (err, data) {
+
+  // If anything went wrong, say so
+  if (err) console.log(err.message);
+
+  // Print the asset information as a raw TransactionOutput object
+  console.log(data);
+
+  // Use the TransactionOutput.toString() method to get a more readable representation
+  console.log(data.toString());
+
+});
